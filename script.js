72c6bbdc38c53243e905b0261f1edd803aeaf5a8
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-11-07 22:49:52 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
-const  todoInput = document.getElementById('todo-input')
-const addTaksButton  = document.getElementById('add-tesk-btn')
-const todoList = document.getElementById('todo-list')
-
-let task = []
-
-addTaksButton.addEventListener('click' ,() => {
-
-const taskText = todoInput.value.trim()//
-if(taskText === "") return;
-
-const newTask = {
-    id:  Date.now(),
-    text: taskText,
-    completed: false 
-}
-task.push(newTask);
-saveTasks();
-todoInput.value = ""// clear input
-console.log(task)
-});
-
-function renderTasks(task)
-{
-   console.log(task)
-}
+document.addEventListener('DOMContentLoaded', () =>{
 
 
-function saveTasks()
-{
+    const  todoInput = document.getElementById('todo-input')
+    const addTaksButton  = document.getElementById('add-tesk-btn')
+    const todoList = document.getElementById('todo-list')
+    
+    let task = JSON.parse(localStorage.getItem('task')) || [];
 
- localStorage.setItem('task',JSON.stringify(task));
+    task.forEach(task => renderTasks(task));
+    
+    addTaksButton.addEventListener('click' ,() => {
+    
+    const taskText = todoInput.value.trim()//
+    if(taskText === "") return;
+    
+    const newTask = {
+        id:  Date.now(),
+        text: taskText,
+        completed: false 
+    }
+    task.push(newTask);
+    saveTasks();
+    todoInput.value = ""// clear input
+    console.log(task)
+    });
+    
+    function renderTasks(task)
+    {
+       console.log(task)
+    }
+    
+    
+    function saveTasks()
+    {
+    
+     localStorage.setItem('task',JSON.stringify(task));
+    
+    
+    }
 
-
-}
+})
