4d72179047759b0e2a4d48c93fc40588f42d68de
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-11-07 21:45:31 +0600

    a

---
+++ script.js
@@ -0,0 +1,12 @@
+const  todoInput = document.getElementById('todo-input')
+const addTaksButton  = document.getElementById('add-tesk-btn')
+const todoList = document.getElementById('todo-list')
+
+let task = []
+
+addTaksButton.addEventListener('click' ,() => {
+
+
+
+
+})
