fb923f5f35997c10db826fa3564dc0cbd002835d
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-11-07 22:04:29 +0600

    a

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ let task = []
 
 addTaksButton.addEventListener('click' ,() => {
 
+const taskText = todoInput.value.trim()//
+if(taskText === "") return;
 
-
-
-})
+const newTask = {
+    id:  Date.now(),
+    text: taskText,
+    completed: false 
+}
+task.push(newTask)
+todoInput.value = ""// clear input
+console.log(task)
+});
